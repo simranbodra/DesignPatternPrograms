fa4ef4a288d5403f517f74833cb688d3c0f5ef2a
simranbodra <simranbodra6@gmail.com>
2018-06-20 18:16:59 +0530

    added visitor pattern program

---
+++ src/com/bridgelabz/design/visitor/ItemElement.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.design.visitor;
+
+public interface ItemElement {
+
+	public int accept(ShoppingCartVisitor visitor);
+}
---
+++ src/com/bridgelabz/design/visitor/ShoppingCartVisitor.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.design.visitor;
+
+public interface ShoppingCartVisitor {
+
+	int visit(Book book);
+	int visit(Fruit fruit);
+}
